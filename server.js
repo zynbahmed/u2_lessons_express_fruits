97351db3f9d0bbe1844fc27a74291a720a2667d5
zynbahmed <201901241@student.polytechnic.bh>
2024-01-31 11:40:22 +0300

    setting up

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+
+//routes
+
+app.get('/ping', (req, res) => {
+    res.json('pong');
+})
+
+
+
+
 app.listen(PORT, () => console.log(`Serving up delicious fruits on port ${PORT} 🍒`))
